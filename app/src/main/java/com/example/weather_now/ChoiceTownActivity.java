f330365ea44676df99d8b29b5f76072148ce93f3
AlexeyAntipov728 <62210339+AlexeyAntipov728@users.noreply.github.com>
2020-08-12 20:36:01 +0300

    lesson_1

---
+++ app/src/main/java/com/example/weather_now/ChoiceTownActivity.java
@@ -0,0 +1,12 @@
+package com.example.weather_now;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChoiceTownActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice_town);
+    }
+}
